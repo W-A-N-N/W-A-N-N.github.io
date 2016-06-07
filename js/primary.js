d1d28295e5e97dfcd1c4de28388d61526c6b3a10
W-A-N-N <johnj@journalist.com>
2016-06-07 16:59:55 +0100

    Update primary.js

--- js/primary.js
+++ js/primary.js
@@ -1,1 +1,13 @@
 
+$.ajax({
+  url: "newsfeed.xml",
+  datatype: "xml",
+  
+  success: function(data){
+    
+  }
+  
+  error: function(data){
+    $(".feed").append("<h2>Sorry, there was an error fetching the feed</h2>")
+  }
+})
