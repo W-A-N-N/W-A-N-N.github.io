65972b5b34c0c45b05c3d902662bf45624975d87
W-A-N-N <johnj@journalist.com>
2016-06-07 17:33:52 +0100

    Update primary.js

--- js/primary.js
+++ js/primary.js
@@ -4,7 +4,21 @@ $.ajax({
   datatype: "xml",
   
   success: function(data){
-    
+    $(data).find("wannfeed item").each(function(){
+      var header = $(this).find("header").text();
+      var story = 0
+      $(data).find("wannfeed item story").each(function(){
+        var title = $(this).find("title").text();
+        var desc = $(this).find("desc").text();
+        story = story + 1
+      });
+      if(story > 1){
+        
+      }
+      else{
+        $(".feed").prepend
+      }
+    })
   }
   
   error: function(data){
