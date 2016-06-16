4e858a4c8f0cd2238a04075c68f034648897ae20
W-A-N-N <johnj@journalist.com>
2016-06-16 17:37:37 +0100

    Create search.js

---
+++ js/search.js
@@ -0,0 +1,12 @@
+function getUrlVars() {
+var vars = {};
+var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+vars[key] = value;
+});
+return vars;
+}
+
+var searchterm = getUrlVars()["search"]
+
+$(".feed").empty()
+$(".feed").prepend(searchterm)
