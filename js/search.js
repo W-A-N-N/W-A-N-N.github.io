06256f58048862edda0b87f087b839e024a396e4
W-A-N-N <johnj@journalist.com>
2016-06-18 12:11:23 +0100

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,12 +1,21 @@
-function getUrlVars() {
-var vars = {};
-var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
-vars[key] = value;
+function search(term){
+$.ajax({
+  url: "newsfeed.xml",
+  datatype: "xml",
+  
+  success: function(data){
+    $(data).find("wannfeed item").each(function(){
+          var header = $(this).find("header").text()
+          var desc = $(this).find("story desc p").html();
+          pos = term.test(String(desc));
+          if(pos = true){
+            $(".results").append("<div class='panel panel-primary' id='"+id+"'><div class='panel-heading'></div><div class='panel-body'></div><div class='panel-footer'><em>Search result for <strong>"+term+"</strong>.</em></div></div>")
+          }
+    });
+  },
+  
+  error: function(data){
+    $(".results").append("<div class='panel panel-danger'><div class='panel-heading'><h2>Sorry, there was an error fetching the feed</h2></div><div class='panel-body'><p>Sorry, looks like I've done something wrong! Please try again later. If this error persists please report an issue <a href='https://github.com/W-A-N-N/W-A-N-N.github.io'>here</a></p></div></div>")
+  },
 });
-return vars;
 }
-
-var searchterm = getUrlVars()["search"]
-
-$(".feed").empty()
-$(".feed").prepend(searchterm)
