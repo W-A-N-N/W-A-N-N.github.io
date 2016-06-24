1ce49dfa1b96b1444d8db75829ae0afbb3bf8bb9
W-A-N-N <johnj@journalist.com>
2016-06-24 14:39:10 +0100

    Update search.js

--- js/search.js
+++ js/search.js
@@ -6,16 +6,16 @@ function search(term){
    success: function(data){ 
      $(data).find("wannfeed item").each(function(){ 
        var header = $(this).find("header").text(); 
-       var story = 0 
+       var story = 0; 
        var id = $(this).attr("no"); 
-       var regTerm = new RegExp(term, 'i');
+       var regTerm = new RegExp(term, "i");
        $(".results").append("<div id='"+id+"' class='panel panel-primary'></div>"); 
        $("#" + id).append("<div class='panel-heading'><h2>"+header+"</h2></div>"); 
 
                  $(this).find("story").each(function(){ 
                  var title = $(this).find("title").text(); 
                  var desc = $(this).find("desc p").html(); 
-                  story = story + 1 
+                  story = story + 1;
                   var title_contains = regTerm.test(title);
                   var desc_contains = regTerm.test(desc);
                   
@@ -28,8 +28,8 @@ function search(term){
   }, 
    
    error: function(data){ 
-     $(".feed").append("<h2>Sorry, there was an error fetching the feed</h2>") 
-   }, 
+     $(".feed").append("<h2>Sorry, there was an error fetching the feed</h2>"); 
+   },
  }); 
 
  		
