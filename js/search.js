fb92b7fbf55451e8e477fb8dd05655f8350e1368
W-A-N-N <johnj@journalist.com>
2016-06-24 12:07:29 +0100

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,42 +1,47 @@
 function search(term){
-  var results;
-$.ajax({
-  url: "newsfeed.xml",
-  datatype: "xml";
-  success: function(data){
-    $(data).find("wannfeed item").each(function(){
-      var results = "<button class='btn btn-lg btn-danger' id='clear'>Clear Search</div>";
-      var id = "search-" + $(this).attr("id");
-      var idStem = $(this).attr("id");
-          var header = $(this).find("header").text();
-          $(this).find("story").each(function(){
-            var searchId = $(this).attr("id");
-            var title = $(this).find("title").text();
-            var desc = $(this).find("desc p").html();
-            pos = term.test(String(desc));
-            if(pos = true){
-              results = results + "<div class='"+storyId+"'><strong>"+title+"</strong><br>"+desc+"</div>";
-            }
-            else{
-              pos = term.test(title);
-              if(pos == true){
-                results = results + "<div class='"+storyId+"'><strong>"+title+"</strong><br>"+desc+"</div>";
-              }
-            }
-          });
-        $(".results").append("<div class='panel panel-primary' id='"+id+"'><div class='panel-heading'><a href='"+idStem+"' stlye='text-decoration:none;'>"+header+"</a></div><div class='panel-body'>"+results+"</div><div class='panel-footer'><em>Search result for <strong>"+term+"</strong>.</em></div></div>")
-
-    });
-  },
+		var results;
+		$.ajax({
+  			url: "newsfeed.xml",
+  			datatype: "xml",
+  			success: function(data){
+    				$(data).find("wannfeed item").each(function()
+    				var idStem = $(this).attr("id");
+    				var id = "search-"+idStem;
+				var results;
+          			var header = $(this).find("header").text();
+          			$(this).find("story").each(function(){
+            				var storyId = $(this).attr("id");
+            				var title = $(this).find("title").text();
+            				var desc = $(this).find("desc p").html();
+            				var clean_term = new RegExp(term, "i");
+            				var pos = clean_term.test(String(desc));
+            				if (pos = true) {
+              					results = results + "<div class='"+storyId+"'><strong>"+title+"</strong><br>"+desc+"</div>";
+            				} else {
+              					pos = clean_term.test(title);
+              					if(pos == true){
+                					results = results + "<div class='"+storyId+"'><strong>"+title+"</strong><br>"+desc+"</div>";
+              					}
+              					else{
+              						results = results + "?";
+              					}
+            				}
+          			});
+        			$(".results").append("<div class='panel panel-primary' id='"+id+"'><div class='panel-heading'><h2 color='white'><a href='"+idStem+"'>"+header+"</a></h2></div></div>")
+        			$("#"+id).append("<div class='panel-body'>"+results+"</div>");
+        			$("#"+id).append("<div class='panel-footer'><em>Search results for <strong>"+term+"</strong></em></div>");
+    				});
+    				if (results !== null){
+  					return true;
+  				} else {
+  					return false;
+				}
+  			},
   
-  error: function(data){
-    $(".results").append("<div class='panel panel-danger'><div class='panel-heading'><h2>Sorry, there was an error fetching the feed</h2></div><div class='panel-body'><p>Sorry, looks like I've done something wrong! Please try again later. If this error persists please report an issue <a href='https://github.com/W-A-N-N/W-A-N-N.github.io'>here</a></p></div></div>")
-  },
-});
+  			error: function(data){
+				$(".results").append("<div class='panel panel-danger'><div class='panel-heading'><h2>Sorry, there was an error fetching the feed</h2></div><div class='panel-body'><p>Sorry, looks like I've done something wrong! Please try again later. If this error persists please report an issue <a href='https://github.com/W-A-N-N/W-A-N-N.github.io'>here</a></p></div></div>")
+  			},
+		});
+	}
 
-if(results == "<button class='btn btn-lg btn-danger' id="clear">Clear Search</div>")
-  return false;
-}
-else {
-  return true;
-}
+ 	
