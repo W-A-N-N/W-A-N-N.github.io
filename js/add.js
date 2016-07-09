53461125e1eba0b5f85bf6eefaa10968f7862cfb
W-A-N-N <johnj@journalist.com>
2016-07-09 14:58:30 +0100

    Update add.js

--- js/add.js
+++ js/add.js
@@ -1,1 +1,14 @@
+function validate(pKey, prKey){
+  var stgA = pKey + prKey
+  var stgB = stgA * stgA
+  var stgC = stgB++
+  if(stgC == prKey){
+    return true;
+  } else {
+    return false;
+  }
+}
 
+function add(name, region, leader, key, key2, members, allies, enemy, neutral){
+  var key = validate(key, getkey(key2));
+}
