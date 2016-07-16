4f8a3bafe3f83ed497f60f112626e17dad890ce0
W-A-N-N <johnj@journalist.com>
2016-07-16 12:10:56 +0100

    Update links.js

--- js/links.js
+++ js/links.js
@@ -3,26 +3,28 @@
         		switch(id){
         			case "Sky Drifters":
         				return "Day0x";
-        			case "Blacklance":
-        				return "Ryulord";
         			case "Traders Of the Foundation":
         				return "BobioTheHobo";
         			case "RageQuit":
         				return "TimSoulSurfer";
         			case "The Anchors of Hummanity":
         				return "MeisterKeen";
-        			case "The Spiders Nest":
-        				return "TheRecluse";
         			case "Enkidu":
         				return "Enin";
         			case "Oblitus Est Spes":
         				return "JPB"
-        			case "The Core Techs":
-        				return "ThirtiethElement";
         			case "The Library":
         				return "Clingbeetle";
         			case "Orchestra":
         			    return "Xerzie";
+        			 case "The Sky Armada":
+        			   return "SonofEarl";
+        			 case "Wander":
+        			   return "Moot";
+        			 case "Timeless":
+        			   return "Natony_123";
+        			 case "Aviator Innovators":
+        			   return "YakkoForever";
         			default:
         				return "404";
         		};
@@ -30,24 +32,18 @@
         		switch(id){
         		        case "The Sky Drifters":
         				return "https://www.worldsadrift.com/alliances/sky-drifters/";
-        			case "Blacklance":
-        				return "https://www.worldsadrift.com/alliances/blackance/";
         			case "Traders Of the Foundation":
         				return "https://www.worldsadrift.com/alliances/traders-of-the-foundation/";
-        			case "RageQuit":
-        				return "https://www.worldsadrift.com/alliances/ragequit/";
         			case "The Anchors of Hummanity":
         				return "https://www.worldsadrift.com/alliances/the-harbormen/";
-        			case "The Spiders Nest":
-        				return "https://www.worldsadrift.com/alliances/the-spiders-nest/";
-        			case "Enkidu":
-        				return "https://www.worldsadrift.com/alliances/enkidu/";
         			case "Oblitus Est Spes":
         				return "https://www.worldsadrift.com/alliances/oblitus-est-spes/"
-        			case "The Core Techs":
-        				return "#404";
         			case "The Library":
         				return "https://www.worldsadrift.com/alliances/the-library/";
+        				case "The Sky Armada":
+        				  return "https://www.worldsadrift.com/alliances/the-glorious-sky-armada/";
+        				case "Aviator Innovators":
+        				  return "https://www.worldsadrift.com/alliances/aviator-innovators/";
         			default:
         				return "https://www.worldsadrift.com/alliances/"+id+"/";
         		};
